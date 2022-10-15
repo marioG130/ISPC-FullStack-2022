7c58b8db2042c9283652168f537b2096ae7dde18
marioG130 <mguerra13@gmail.com>
2022-10-15 20:19:50 -0300

    diagrama DER

---
+++ backend/design/generate-python/clases/Cliente.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Cliente:
+    def __init__(self):
+        self.DNI = None
+        self.apellido = None
+        self.nombre = None
+        self.direccion = None
+        self.telefono = None
+        self.email = None
+        self.fechaNac = None
+
